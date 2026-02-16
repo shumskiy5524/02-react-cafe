75285321f00c86bd60a4077c4eb92d4a06b5ed48
shumskiy <shumskiparents@gmail.com>
2026-02-16 20:49:35 +0100

    all task

---
+++ src/components/Product.tsx
@@ -0,0 +1,10 @@
+// src/components/Product.tsx
+
+export default function Product() {
+  return (
+    <div>
+      <h2>Tacos</h2>
+      <p>Price: 999 credits</p>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+import App from "./components/App.tsx";
+
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
